9017450e96f538ec1af5499c2da079d05f6609b2
Udi Oron <udioron@gmail.com>
2013-12-16 15:10:19 +0200

    Nice and basic admin

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,20 @@
+from ads import models
 from django.contrib import admin
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+
+
+class AdAdmin(admin.ModelAdmin):
+    list_display = (
+                    "title",
+                    "company_name",
+                    "created_at",
+                    "geographic_area",
+                    "tag_list",
+                    )
+
+
+admin.site.register(models.Tag, TagAdmin)
+admin.site.register(models.Ad, AdAdmin)
--- ads/models.py
+++ ads/models.py
@@ -5,6 +5,9 @@ class Tag(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.CharField(max_length=200, unique=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Ad(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,4 +19,11 @@ class Ad(models.Model):
     geographic_area = models.CharField(max_length=200, null=True,
                                        blank=True)
 
-    tags = models.ManyToManyField(Tag, related_name="ads")
+    tags = models.ManyToManyField(Tag, related_name="ads", blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def tag_list(self):
+        return ", ".join([tag.name for tag in self.tags.all()])
+    tag_list.short_description = "Tags"
