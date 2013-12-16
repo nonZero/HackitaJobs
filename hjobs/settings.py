8e1670e6ade4a5cf1ffda542f2f9b7d718020a02
Udi Oron <udioron@gmail.com>
2013-12-16 16:57:02 +0200

    Added login/logout and base template

--- hjobs/settings.py
+++ hjobs/settings.py
@@ -48,7 +48,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
+TEMPLATE_CONTEXT_PROCESSORS = (
+                            "django.contrib.auth.context_processors.auth",
+                            "django.core.context_processors.request",
+                            "django.core.context_processors.debug",
+                            "django.core.context_processors.i18n",
+                            "django.core.context_processors.media",
+                            "django.core.context_processors.static",
+                            "django.core.context_processors.tz",
+                            "django.contrib.messages.context_processors.messages"
+                            )
 ROOT_URLCONF = 'hjobs.urls'
 
 WSGI_APPLICATION = 'hjobs.wsgi.application'
@@ -82,3 +91,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
--- hjobs/urls.py
+++ hjobs/urls.py
@@ -1,6 +1,8 @@
 from ads import views
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
 
 admin.autodiscover()
 
@@ -11,8 +13,17 @@ urlpatterns = patterns('',
     url(r'^ad/(\d+)/$', views.ad, name='ad'),
 
     url(r'^post-ad/$',
-        views.PostAdView.as_view(),
+        login_required(views.PostAdView.as_view()),
         name='post_ad'),
+
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}, name="login"),
+
+    url(r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {'next_page': reverse_lazy('home')}, name="logout"),
+
 
     url(r'^admin/', include(admin.site.urls)),
 )
